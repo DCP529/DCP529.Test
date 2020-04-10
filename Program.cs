9ea1874c329aefe11fe2ec2463073900ae46dd83
DCP529 <61003286+DCP529@users.noreply.github.com>
2020-04-10 15:06:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число от 0 до 10 для умножения");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число от 0 до 10 для умножения");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+
+            if(x >= 0 && x <=10)
+            {
+                Console.WriteLine("");
+                
+            }
+            else
+            {
+
+            }
+
+
+
+
+        }
+
+
+    }
+}
